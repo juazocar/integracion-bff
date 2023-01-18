1ecba3d388555f33de15382ad2fee7d9ff5eb821
jazocar <jazocar@gasco.cl>
2023-01-18 16:01:00 -0300

    first commit

---
+++ src/main/java/cl/duoc/integracionbff/IntegracionBffApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.integracionbff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegracionBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegracionBffApplication.class, args);
+	}
+
+}
